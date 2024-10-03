6b08b3c158bcee6448e687dcd9bcd1de5e755f6b
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2024-10-03 18:45:59 +0000

    commit1

---
+++ practicas/pact4/detalle.php
@@ -0,0 +1,97 @@
+<?php
+ if(isset($_GET['nombre']) && isset($_GET['imagen']) && isset($_GET['horario']) 
+ && isset($_GET['sinopsis']) && isset($_GET['reparto']) && isset($_GET['director'])
+ && isset($_GET['genero']) && isset($_GET['calificacion']) && isset($_GET['trailer']) && isset($_GET['duracion'])){
+     //pillardatos
+     $nombrePeli = $_GET['nombre'];
+     $imagenLink = $_GET['imagen'];
+     $horarioPeli = $_GET['horario'];
+     $sinopsisPeli = $_GET['sinopsis'];
+     $repartoPeli = $_GET['reparto'];
+     $directorPeli = $_GET['director'];
+     $generoPeli = $_GET['genero'];
+     $calificacionPeli = $_GET['calificacion'];
+     $trailerPeli = $_GET['trailer'];
+     $duracionPeli = $_GET['duracion'];};
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <style>
+        .grande{
+            border: 1px solid black;
+        }
+        .principal{
+            
+            display: flex;
+        }
+        .izquierda{
+            margin: 10px;
+        }
+        .derecha{
+            width: 100%;
+        }
+        .trailer{
+            border: 1px solid black;
+            margin: 10px;
+            padding:10px ;
+            justify-content: center;
+            display: flex;
+            flex-direction: column;
+            align-items: center;
+            border-radius: 45;
+            
+        }
+        .horario{
+            margin: 10px;
+            padding: 10px;
+            border: 1px solid black;
+            width: 100%;
+            justify-content: space-around;
+            display: flex;
+            flex-direction: row;
+            align-items: start;
+        }
+        .titulo{
+            margin: 10px;
+            padding: 10px;
+        }
+        .imagen{
+            width: 400px;
+            height: 600px;
+        }
+    </style>
+</head>
+<body>
+        <?php
+            echo '
+            <div class="grande">
+        <h1 class="titulo"> ' . $_GET['nombre'] .' </h1>
+        <div class="principal">
+            <div class="izquierda">
+                <img class="imagen" src="'.$_GET['imagen'].'" alt="img">
+                <p class="trailer"> ▶️ Trailer</p>
+            </div>
+            <div class="derecha">
+                <p>'. $_GET['sinopsis'].'</p>
+                <p><strong>Duracion</strong> '. $_GET['duracion'] . '</p>
+                <p><strong>Director</strong> '. $_GET['director'].'</p>
+                <p><strong>Reparto</strong> '. $_GET['reparto'].'</p>
+                <p><strong>Calificacion</strong> '. $_GET['calificacion'].'</p>
+                <p><strong>Genero</strong> '. $_GET['genero'].'</p>
+                <div>
+                    <p class="horario"><strong>Horario</strong>'. $_GET['horario'] .'</p>
+                </div>
+            </div>
+        </div>
+    </div>
+            ';
+
+        ?>
+
+    
+</body>
+</html>
---
+++ practicas/pact4/peliculas.php
@@ -0,0 +1,262 @@
+<?php
+$peliculas = [
+  [
+      "nombre" => "Inception",
+      "imagen" => "https://m.media-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_.jpg",
+      "horario" => "21:00",
+      "sinopsis" => "Un ladrón que roba secretos del subconsciente es dado la oportunidad de borrar su pasado criminal.",
+      "duracion" => "148 minutos",
+      "director" => "Christopher Nolan",
+      "reparto" => "Leonardo DiCaprio, Joseph Gordon-Levitt, Ellen Page",
+      "calificacion" => "PG-13",
+      "genero" => "Ciencia Ficción, Acción",
+      "trailer" => "https://www.youtube.com/watch?v=8hP9D6kZseM"
+  ],
+  [
+      "nombre" => "The Shawshank Redemption",
+      "imagen" => "https://m.media-amazon.com/images/M/MV5BMDAyY2FhYjctNDc5OS00MDNlLThiMGUtY2UxYWVkNGY2ZjljXkEyXkFqcGc@._V1_.jpg",
+      "horario" => "18:00",
+      "sinopsis" => "Dos hombres forman una amistad durante su tiempo en prisión.",
+      "duracion" => "142 minutos",
+      "director" => "Frank Darabont",
+      "reparto" => "Tim Robbins, Morgan Freeman",
+      "calificacion" => "R",
+      "genero" => "Drama",
+      "trailer" => "https://www.youtube.com/watch?v=6hB3S9bIaco"
+  ],
+  [
+      "nombre" => "The Dark Knight",
+      "imagen" => "https://m.media-amazon.com/images/M/MV5BMTMxNTMwODM0NF5BMl5BanBnXkFtZTcwODAyMTk2Mw@@._V1_.jpg",
+      "horario" => "20:30",
+      "sinopsis" => "Batman se enfrenta al Joker, quien busca sembrar el caos en Gotham.",
+      "duracion" => "152 minutos",
+      "director" => "Christopher Nolan",
+      "reparto" => "Christian Bale, Heath Ledger",
+      "calificacion" => "PG-13",
+      "genero" => "Acción, Crimen, Drama",
+      "trailer" => "https://www.youtube.com/watch?v=EXeTwQWrcwY"
+  ],
+  [
+      "nombre" => "Forrest Gump",
+      "imagen" => "https://pics.filmaffinity.com/Forrest_Gump-212765827-large.jpg",
+      "horario" => "19:00",
+      "sinopsis" => "La vida de un hombre con un bajo coeficiente intelectual y su impacto en la historia de EE. UU.",
+      "duracion" => "142 minutos",
+      "director" => "Robert Zemeckis",
+      "reparto" => "Tom Hanks, Robin Wright",
+      "calificacion" => "PG-13",
+      "genero" => "Drama, Romance",
+      "trailer" => "https://www.youtube.com/watch?v=bLvqoHBptdg"
+  ],
+  [
+      "nombre" => "The Matrix",
+      "imagen" => "https://pics.filmaffinity.com/the_matrix-155050517-mmed.jpg",
+      "horario" => "22:00",
+      "sinopsis" => "Un hacker descubre la verdad sobre su realidad y su papel en la guerra contra sus opresores.",
+      "duracion" => "136 minutos",
+      "director" => "Lana Wachowski, Lilly Wachowski",
+      "reparto" => "Keanu Reeves, Laurence Fishburne",
+      "calificacion" => "R",
+      "genero" => "Ciencia Ficción, Acción",
+      "trailer" => "https://www.youtube.com/watch?v=vKQi0rER4tI"
+  ],
+  [
+      "nombre" => "The Godfather",
+      "imagen" => "https://static.wikia.nocookie.net/doblaje/images/9/9a/Elpadrino.jpg/revision/latest?cb=20211023042804&path-prefix=es",
+      "horario" => "23:00",
+      "sinopsis" => "La historia de la familia Corleone y su imperio del crimen en Nueva York.",
+      "duracion" => "175 minutos",
+      "director" => "Francis Ford Coppola",
+      "reparto" => "Marlon Brando, Al Pacino",
+      "calificacion" => "R",
+      "genero" => "Crimen, Drama",
+      "trailer" => "https://www.youtube.com/watch?v=sY1S34973zA"
+  ],
+  [
+      "nombre" => "Pulp Fiction",
+      "imagen" => "https://pics.filmaffinity.com/pulp_fiction-210382116-mmed.jpg",
+      "horario" => "21:30",
+      "sinopsis" => "Las vidas de dos asesinos a sueldo, un boxeador, la esposa de un gánster y un par de criminales se entrelazan.",
+      "duracion" => "154 minutos",
+      "director" => "Quentin Tarantino",
+      "reparto" => "John Travolta, Uma Thurman, Samuel L. Jackson",
+      "calificacion" => "R",
+      "genero" => "Crimen, Drama",
+      "trailer" => "https://www.youtube.com/watch?v=s7EdQ4FqbhY"
+  ],
+  [
+      "nombre" => "Interstellar",
+      "imagen" => "https://pics.filmaffinity.com/Interstellar-832932589-large.jpg",
+      "horario" => "20:00",
+      "sinopsis" => "Un grupo de exploradores viaja a través de un agujero de gusano en el espacio.",
+      "duracion" => "169 minutos",
+      "director" => "Christopher Nolan",
+      "reparto" => "Matthew McConaughey, Anne Hathaway",
+      "calificacion" => "PG-13",
+      "genero" => "Ciencia Ficción, Aventura",
+      "trailer" => "https://www.youtube.com/watch?v=zSWdZVtXT7E"
+  ],
+  [
+      "nombre" => "Fight Club",
+      "imagen" => "https://m.media-amazon.com/images/I/61IgtYrLF5L._AC_UF894,1000_QL80_.jpg",
+      "horario" => "22:30",
+      "sinopsis" => "Un hombre insatisfecho con su vida crea un club de lucha clandestino.",
+      "duracion" => "139 minutos",
+      "director" => "David Fincher",
+      "reparto" => "Brad Pitt, Edward Norton",
+      "calificacion" => "R",
+      "genero" => "Drama, Suspense",
+      "trailer" => "https://www.youtube.com/watch?v=8F9pW9T43vU"
+  ],
+];
+
+
+
+
+
+
+?>
+<!doctype html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="description" content="">
+  <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
+  <meta name="generator" content="Hugo 0.72.0">
+  <title>Album example · Bootstrap</title>
+
+  <link rel="canonical" href="https://v5.getbootstrap.com/docs/5.0/examples/album/">
+
+
+
+  <!-- Bootstrap core CSS -->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/css/bootstrap.min.css" integrity="sha384-r4NyP46KrjDleawBgD5tp8Y7UzmLA05oM1iAEQ17CSuDqnUK2+k9luXQOfXJCJ4I" crossorigin="anonymous">
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/5.0.0-alpha1/js/bootstrap.min.js" integrity="sha384-oesi62hOLfzrys4LxRF63OJCXdXDipiYWBnvTl9Y9/TRlw5xlKIEHpNyvvDShgf/" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+
+  <style>
+    .bd-placeholder-img {
+      font-size: 1.125rem;
+      text-anchor: middle;
+      -webkit-user-select: none;
+      -moz-user-select: none;
+      -ms-user-select: none;
+      user-select: none;
+    }
+
+    @media (min-width: 768px) {
+      .bd-placeholder-img-lg {
+        font-size: 3.5rem;
+      }
+    }
+  </style>
+
+
+</head>
+
+<body>
+
+  <header>
+    <div class="collapse bg-dark" id="navbarHeader">
+      <div class="container">
+        <div class="row">
+          <div class="col-sm-8 col-md-7 py-4">
+            <h4 class="text-white">About</h4>
+            <p class="text-muted">Add some information about the album below, the author, or any other background
+              context. Make it a few sentences long so folks can pick up some informative tidbits. Then, link them off
+              to some social networking sites or contact information.</p>
+          </div>
+          <div class="col-sm-4 offset-md-1 py-4">
+            <h4 class="text-white">Contact</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" class="text-white">Follow on Twitter</a></li>
+              <li><a href="#" class="text-white">Like on Facebook</a></li>
+              <li><a href="#" class="text-white">Email me</a></li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="navbar navbar-dark bg-dark shadow-sm">
+      <div class="container">
+        <a href="#" class="navbar-brand d-flex align-items-center">
+          <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="none" stroke="currentColor"
+            stroke-linecap="round" stroke-linejoin="round" stroke-width="2" aria-hidden="true" class="mr-2"
+            viewBox="0 0 24 24">
+            <path d="M23 19a2 2 0 0 1-2 2H3a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h4l2-3h6l2 3h4a2 2 0 0 1 2 2z" />
+            <circle cx="12" cy="13" r="4" /></svg>
+          <strong>Album</strong>
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader"
+          aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+      </div>
+    </div>
+  </header>
+
+  <main>
+
+    <section class="py-5 text-center container">
+      <div class="row py-lg-5">
+        <div class="col-lg-6 col-md-8 mx-auto">
+          <h1 class="font-weight-light">Cartelera Vista de Erizo</h1>
+          <p class="lead text-muted">Esta es nuestra cartelera</p>
+          <p>
+            <a href="#" class="btn btn-primary my-2">Main call to action</a>
+            <a href="#" class="btn btn-secondary my-2">Secondary action</a>
+          </p>
+        </div>
+      </div>
+    </section>
+
+    <div class="album py-5 bg-light">
+      <div class="container">
+        <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+          <?php
+          foreach($peliculas as $pelicula){
+            echo '<div class="col">
+            <div class="card shadow-sm">
+              <img class="bd-placeholder-img card-img-top" width="100%" height="225" src='. $pelicula['imagen'].' alt="" >
+              <div class="card-body">
+                <h1>'. $pelicula['nombre'].'</h1>
+                <p class="card-text">' . $pelicula['horario'].'</p>
+                <div class="d-flex justify-content-between align-items-center">
+                  <div class="btn-group">
+                    <button type="button" class="btn btn-sm btn-outline-secondary">Ver Trailer</button>
+                    <a href="detalle.php?calificacion='.$pelicula['calificacion'].'&genero='.$pelicula['genero'].'&reparto='.$pelicula['reparto'].'&director='.$pelicula['director'].'&nombre='.$pelicula['nombre'].'&sinopsis=' . $pelicula['sinopsis'] .'&imagen='.$pelicula['imagen'].'&horario='.$pelicula['horario'].'&duracion='.$pelicula['duracion'].'"><button type="button" class="btn btn-sm btn-outline-secondary">Mas Informacion</button></a>
+                  </div>
+                  <small class="text-muted"></small>
+                </div>
+              </div>
+            </div>
+          </div>
+            ';
+          };
+
+          
+            
+          ?>
+          
+        </div>
+      </div>
+    </div>
+
+  </main>
+
+  <footer class="text-muted py-5">
+    <div class="container">
+      <p class="float-right mb-1">
+        <a href="#">Back to top</a>
+      </p>
+      <p class="mb-1">Album example is &copy; Bootstrap, but please download and customize it for yourself!</p>
+      <p class="mb-0">New to Bootstrap? <a href="/">Visit the homepage</a> or read our <a
+          href="/docs/5.0/getting-started/introduction/">getting started guide</a>.</p>
+    </div>
+  </footer>
+
+</body>
+
+</html>
