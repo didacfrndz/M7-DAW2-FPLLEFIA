6400de32740804584360e3bad6db53d9083e68bf
didacfrndz <156692497+didacfrndz@users.noreply.github.com>
2025-09-15 15:37:32 +0000

    primercommit

---
+++ practicas/pp1-primera-app/hola.php
@@ -0,0 +1,33 @@
+<?php 
+function head(){
+    echo '';
+}
+function explicacion(){
+    echo 'texto';
+}
+function footer(){
+    echo 'footer';
+}
+
+?>
+<html>
+    <head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+        
+    </head>
+    <body class="">
+        <header class="">
+            <img class="" src="assets/logo-fpllefia.jpg" alt="logofpllefia">
+            <h1 class="">Modulo 7 Practica 1. Mi primera aplicacion en php</h1>
+        </header>
+        <img src="assets/erizo.jpg" alt="logofpllefia">
+        <p>En la señalizacion del codigo presentado podemos ver el como damos pie al PHP utilizando "<?php ?>" para abrir y cerrar </p>
+        <footer>
+
+        </footer>
+    </body>
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+    
+</html>
